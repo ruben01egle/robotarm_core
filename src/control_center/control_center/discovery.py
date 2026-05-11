182dcb04d4d5c9384e242b64bf6f46d7e8f4bb52
Ruben Egle <ruben01egle@gmail.com>
2026-05-11 03:24:31 +0200

    supressing middle ware warn for robot launch due to micro ros limits

--- src/control_center/control_center/discovery.py
+++ src/control_center/control_center/discovery.py
@@ -1,7 +1,6 @@
 import socket
 import time
 
-# Wir senden AN Port 8888, wir binden uns aber selbst NICHT daran
 PORT = 6666 
 SIGNATURE = b"ROS2"
 
@@ -9,7 +8,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while True:
-    # Nur senden! Kein sock.bind()!
     sock.sendto(SIGNATURE, ('<broadcast>', PORT))
     print(f"Beacon gesendet an Port {PORT}...")
     time.sleep(1)
--- src/robot_bringup/launch/launch_robot.py
+++ src/robot_bringup/launch/launch_robot.py
@@ -2,19 +2,39 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    extra_args = ['--log-level', 'rmw_cyclonedds_cpp:=ERROR']
+
     return LaunchDescription([
         
         Node(
             package='control_center',
             executable='control_center_node',
             name='control_center',
-            output='screen'
+            output='screen',
+            ros_arguments=extra_args
         ),
         
         Node(
             package='gui',
             executable='gui_ros_node',
             name='gui_node',
-            output='screen'
+            output='screen',
+            ros_arguments=extra_args
+        ),
+
+        Node(
+            package='mission_controller',
+            executable='mission_controller_node',
+            name='mission_controller_node',
+            output='screen',
+            ros_arguments=extra_args
+        ),
+
+        Node(
+            package='planner',
+            executable='p2p_jointspace_node',
+            name='p2p_jointspace_node',
+            output='screen',
+            ros_arguments=extra_args
         ),
     ])
